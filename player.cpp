1e1084852282ae5bf2038b3572ac6825eb5dd36d
kx6023 <kx6023@gmail.com>
2021-09-03 15:39:02 -0600

    lagged out & started the player class

--- player.cpp
+++ player.cpp
@@ -1,7 +1,19 @@
 #include "player.h"
 
 using namespace std;
+player::player(string team, string name, int age, double heightcm, double weight, 
+        int games, double points, double rebounds, double assists) {
+		this->team = team;
+		this->name = name;
+		this->age = age;
+		this->heightcm = heightcm;
+		this->weight = weight;
+		this->game = game;
+		this->points = points;
+		this->rebounds = rebounds;
+		this->assists = assists;
+}
+player::~player() { }	
 
-player::player() {
-	
+Vector<int> 
 }
--- player.h
+++ player.h
@@ -5,13 +5,26 @@
 
 class player {
 public:
-    player();
-    vector <double> getStats();
+    player(std::string team, std::string name, int age, double heightcm, double weight, 
+        int games, double points, double rebounds, double assists);
 
+    void changeTeam(std::string team);
+    vector <double> getStats();
+    //vector <double> setStats();
+    ~player();
 private:
     std::string team;
-    double weightedInjury;
+    //double weightedInjury;
+    std::string name;
+    int age;
     double heightcm;
     double weight;
+    int games;
+    double points;
+    double rebounds;
+    double assists;
+    //double fgPercent;
+    //double fg3Percent;
+    //double per;
 };
 #endif
