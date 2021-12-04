b2410872b5cd89120884ed35aa146adbea8b4e79
Larry Xu <lhxu@usc.edu>
2021-12-03 20:02:14 -0800

    adjustments to algorithm usage

--- besgitbol.cpp
+++ besgitbol.cpp
@@ -8,12 +8,14 @@
 using namespace std;
 
 //type make
-//then ./bin/besgitbol "Milwaukee Bucks.txt" "hashtable dump test.txt"
+//then ./bin/besgitbol "Milwaukee Bucks.txt" "Golden State Warriors.txt" "hashtable dump test.txt"
 //capitalization matters
 //type make clean to clear executables
 
 //run for team generation + comparison
 //need to upload teams manually
+
+//currently sums aggregates -> want to implement linear regression to predict + consider more stats
 
 int main(int argc, char *argv[])
 {
@@ -32,26 +34,44 @@ int main(int argc, char *argv[])
 	parser.parse("Milwaukee Bucks", bucks.getAllPlayers(), stream);
 	stream.close();
 	vector<string> bucksPlayers = bucks.getRoster();
-	cout << "Bucks" << endl;
+	//cout << "Bucks" << endl;
+	double bucksAggregate = 0;
 	for (unsigned int i = 0; i < bucksPlayers.size(); i++)
 	{
-		cout << bucksPlayers[i] << " Aggregate: " << bucks.getPlayer(bucksPlayers[i])->getAggregate() << endl;
+		//cout << bucksPlayers[i] << " Aggregate: " << bucks.getPlayer(bucksPlayers[i])->getAggregate() << endl;
+		bucksAggregate += bucks.getPlayer(bucksPlayers[i])->getAggregate();
 	}
-	cout << endl;
+	//cout << endl;
 
 	stream.open(argv[2]);
 	team warriors = team("Golden State Warriors");
 	parser.parse("Golden State Warriors", warriors.getAllPlayers(), stream);
 	vector<string> warriorsPlayers = warriors.getRoster();
-	cout << "Warriors" << endl;
+	//cout << "Warriors" << endl;
+	double warriorsAggregate = 0;
 	for (unsigned int i = 0; i < warriorsPlayers.size(); i++)
 	{
-		cout << warriorsPlayers[i] << " Aggregate: " << warriors.getPlayer(warriorsPlayers[i])->getAggregate() << endl;
+		//cout << warriorsPlayers[i] << " Aggregate: " << warriors.getPlayer(warriorsPlayers[i])->getAggregate() << endl;
+		warriorsAggregate += warriors.getPlayer(warriorsPlayers[i])->getAggregate();
+	}
+
+	if (warriorsAggregate > bucksAggregate) {
+		cout << "Warriors are favored to win" << endl;
+	}
+
+	else if (warriorsAggregate < bucksAggregate) {
+		cout << "Bucks are favored to win" << endl;
+	}
+
+	else {
+		cout << "Too close to call" << endl;
 	}
 
 	ofstream dump;
-	dump.open(argv[2]);
+	dump.open(argv[3]);
+	dump << "Bucks:\n";
 	bucks.getAllPlayers()->reportAll(dump);
+	dump << "Warriors:\n";
 	warriors.getAllPlayers()->reportAll(dump);
 
 	return 0;
